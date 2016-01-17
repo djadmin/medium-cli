322e2b20fa19d813a7f30be9eb525464d3f1a75d
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-01-18 03:15:01 +0530

    Make medium article viewable in browser

--- medium-open.js
+++ medium-open.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
+'use strict';
 
-// var program = require('commander');
-var open = require('open');
-var url = 'https://medium.com/@googleforwork/one-googler-s-take-on-managing-your-time-b441537ae037';
-// program
-// 	.arguments('<number>')
-// 	.action(function (id) {
-// 		console.log("id--> " + id );
-// 	})
-// 	.parse(process.argv);
+var program = require('commander');
+var op = require('open');
 
-
-// var number = program.args;
-// console.log(number);
-// console.log(program);
-open(url);
+program
+	.arguments('<number>')
+	.action(function (url) {
+		// TODO : Validate url
+		op(url);
+	})
+	.parse(process.argv);
