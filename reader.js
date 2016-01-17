8845a6da5a71cd28c57574e89335196cc158a0ee
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-01-18 02:59:15 +0530

    Add medium article reader service

---
+++ reader.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var rp = require('request-promise');
+var cheerio = require('cheerio');
+var htmlToText = require('html-to-text');
+var Stream = require('string-stream');
+var pager = require('default-pager');
+var urlParser = require('url');
+
+// var url = 'https://medium.com/@googleforwork/one-googler-s-take-on-managing-your-time-b441537ae037';
+
+function getTextFromHtml(html) {
+	return htmlToText.fromString(html);
+}
+
+function printArticle(content) {
+	var readStream = new Stream(content);
+	readStream.pipe(pager(function () {
+		console.log('(END)');
+	}));
+}
+
+function parseContent(html) {
+	var $ = cheerio.load(html);
+	var articleMarkup = $('main.postArticle-content').html();
+	var content = getTextFromHtml(articleMarkup);
+	return printArticle(content);
+}
+
+function requestContent(url) {
+	return rp(url);
+}
+
+// Show Content to user 
+function showContent(url) {
+	requestContent(url).then(function (body) {
+		var content = parseContent(body);
+		printArticle(content);
+	}, function () {
+		console.log('Oops! Something went wrong!');
+	});
+}
+
+function validateUrl (url) {
+	if (urlParser.parse(url).host === 'medium.com') {
+		return true;
+	}
+	return false;
+}
+
+function show(url) {
+	if (!validateUrl(url)) {
+		console.log('Sorry! Only Medium articles are allowed!');
+		return;
+	}
+	showContent(url);
+}
+
+module.exports = {
+	show: show
+};
