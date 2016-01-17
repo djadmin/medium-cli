2c89aab359bfebd68e7e3419a5ce35827bc8a7ff
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-01-18 02:57:51 +0530

    Add interface to CLI and update commands

--- medium-read.js
+++ medium-read.js
@@ -1,33 +1,13 @@
 'use strict';
 
-var request = require('request');
-var cheerio = require('cheerio');
-var htmlToText = require('html-to-text');
-var Stream = require('string-stream');
-var pager = require('default-pager');
+var program = require('commander');
+var reader = require('./reader');
 
-var url = 'https://medium.com/@googleforwork/one-googler-s-take-on-managing-your-time-b441537ae037';
+// Commander API
+program
+	.arguments('<number>')
+	.action(function (url) {
+		reader.show(url);
+	})
+	.parse(process.argv);
 
-function getTextFromHtml(html) {
-	return htmlToText.fromString(html);
-}
-
-function printArticle(content) {
-	var readStream = new Stream(content);
-	readStream.pipe(pager(function () {
-		console.log('(END)');
-	}));
-}
-
-function processHtml(html) {
-	var $ = cheerio.load(html);
-	var articleMarkup = $('main.postArticle-content').html();
-	var content = getTextFromHtml(articleMarkup);
-	printArticle(content);
-}
-
-request(url, function(err, res, body) {
-	if (!err && res.statusCode == 200) {
-		processHtml(body);
-	}
-});
--- medium.js
+++ medium.js
@@ -1,10 +1,60 @@
 #!/usr/bin/env node
-
+'use strict';
 var path = require('path');
 var pkg = require(path.join(__dirname, 'package.json'));
-
+var post = require('./post');
+var reader = require('./reader');
 // Parse command line options
 var program = require('commander');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+
+//Disable Chalk colors
+// chalk = new chalk.constructor({enabled: false});
+
+function constructChoices(posts) {
+	var choices = [];
+	var space = ' ', separator = '--';
+	posts.forEach(function (post, index) {
+		var line = '';
+		var number = (index + 1) + '.',
+			headline = '"' + post.headline + '"',
+			votes = post.votes + space + 'votes',
+			author = 'by' + space + '@' + post.authorSlug;
+		
+		// construct article message
+		line += chalk.gray(number) + space;
+		line += chalk.yellow(headline) + space;
+		line += chalk.green(separator + votes) + space;
+		line += chalk.cyan(separator + author);
+		
+		var choice = {
+			'name': line,
+			'short': post.headline,
+			'value': post.url
+		};
+		choices.push(choice);
+		// new inquirer.Separator(),
+	});
+	return choices;
+}
+
+function listTrendingPosts(posts) {
+		var choices = constructChoices(posts);
+		inquirer.prompt([
+			{
+				type: 'list',
+				name: 'url',
+				// message: 'Medium - Top Stories',
+				message: 'Select the article to read :',
+				choices: choices,
+				pageSize: 20
+			}
+		], function (answers) {
+			var url = answers.url;
+			reader.show(url);
+		});
+}
 
 program
 	.version(pkg.version)
@@ -15,5 +65,19 @@ program
 	.parse(process.argv);
 
 if (program.number) {
-	console.log('Fetch top %s stories', program.number);
+	post.getTrending(program.number).then(function (posts) {
+		listTrendingPosts(posts);
+	}, function (err) {
+		console.log('Oops! Something went wrong! %s', err);
+	});
+	console.log('Medium - Top %s stories...', program.number);
+}
+
+if (!process.argv.slice(2).length) {
+	console.log('Going into default option');
+	// Default Option
+	post.getTrending().then(function (posts) {
+		listTrendingPosts(posts);
+	});
+	console.log('Fetching top %s stories...', program.number);
 }
